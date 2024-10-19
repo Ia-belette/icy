39d25d97124138776305d7b1cef8a9584afcfd1c
dereje <getutadessedereje@icloud.com>
2024-10-19 20:07:21 +0200

    feat(main): integrate scrapers into main application

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ const query = "Taylor Swift";
 
 const wooka = new WookaFRScraper();
 const guinchez = new GuinchezScraper();
-// git commit -m 'feat(scrapers): add guinchez scraper and refactor existing scrapers' -m 'add new guinchez scraper implementation' -m 'update scrapers index to include new guinchez scraper' -m 'refactor BaseScraper and wookaScraper for consistency' -m 'update main.ts to integrate new scraper'
+
+console.log(await wooka.scrape(query));
+console.log(await guinchez.scrape(query));
