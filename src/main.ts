d761004a8b9228bd7dbefbd3d4bdef9f58d44fdf
dereje <getutadessedereje@icloud.com>
2024-10-21 17:26:22 +0200

    feat(routes): add new route to express application

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
+import express from "npm:express@4.18.2";
+import { router } from "./routes/providers.ts";
+const app = express();
 
+app.use("/", router);
+
+app.listen(8000);
---
+++ src/routes/providers.ts
@@ -0,0 +1,84 @@
+import Router, { Request, Response } from "npm:express@4.18.2";
+import {
+  CoflixScraper,
+  Jour1FilmScraper,
+  MegaStreamScraper,
+  NightFlixScraper,
+  SeeFrenchScraper,
+  GuinchezScraper,
+  PrincessFilmsScraper,
+  SenpaiStreamScraper,
+  TopStreamingScraper,
+  WookaFRScraper,
+  XalaflixScraper,
+} from "../scrapers/index.ts";
+
+const scrapers = {
+  coflix: new CoflixScraper(),
+  jour1film: new Jour1FilmScraper(),
+  nightflix: new NightFlixScraper(),
+  seefrench: new SeeFrenchScraper(),
+  monstream: new MegaStreamScraper(),
+  xalaflix: new XalaflixScraper(),
+  guinchez: new GuinchezScraper(),
+  princessfilms: new PrincessFilmsScraper(),
+  senpai: new SenpaiStreamScraper(),
+  topstreaming: new TopStreamingScraper(),
+  wookafr: new WookaFRScraper(),
+};
+
+const MAX_PROVIDERS = 7;
+
+export const router = Router();
+
+router.get("/search/:query/:provider", async (req: Request, res: Response) => {
+  const query = req.params.query;
+  const providers = req.params.provider.split(",");
+
+  console.log(
+    query,
+    "Recherche pour:",
+    providers,
+    "providers.length",
+    providers.length
+  );
+
+  if (providers.length > MAX_PROVIDERS) {
+    return res.status(400).json({
+      error: `Trop de providers demandés. Maximum autorisé : ${MAX_PROVIDERS}`,
+    });
+  }
+
+  const scraperPromises = providers.map((provider: string) =>
+    callScraper(provider, query)
+      .then((result) => result || [])
+      .catch((error) => {
+        console.error(`Erreur pour ${provider}:`, error);
+        return [];
+      })
+  );
+
+  const results = await Promise.all(scraperPromises);
+  const flattenedResults = results
+    .flat()
+    .filter(
+      (item: {
+        title: string;
+        url: string;
+        image: string;
+        site_name: string;
+      }): item is NonNullable<typeof item> => item != null
+    );
+
+  console.log("Résultats finaux:", flattenedResults);
+  res.json(flattenedResults);
+});
+
+function callScraper(provider: string, query: string) {
+  const scraper = scrapers[provider as keyof typeof scrapers];
+  if (!scraper) {
+    console.warn(`Scraper non implémenté pour ${provider}`);
+    return Promise.resolve([]);
+  }
+  return scraper.scrape(query);
+}
